7b57fdc1934563f8653abe7b945282c8c28fc533
Chengwei Zhang <1535909927@qq.com>
2021-02-06 22:55:32 +0800

    install router.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 // import './plugins/element.js'
 import { Button, Select } from 'element-ui'
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(Button)
 Vue.use(Select)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')

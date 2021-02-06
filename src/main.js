f6e5e4dddcccdb3bb96d883276af0cf3ece040c6
Chengwei Zhang <1535909927@qq.com>
2021-02-06 22:49:54 +0800

    import element.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import './plugins/element.js'
+import { Button, Select } from 'element-ui'
 
 Vue.config.productionTip = false
+Vue.use(Button)
+Vue.use(Select)
 
 new Vue({
   render: h => h(App),

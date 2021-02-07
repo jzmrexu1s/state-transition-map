6c51d753ff30a7c4f3584a4c742826887e105556
Chengwei Zhang <1535909927@qq.com>
2021-02-07 21:44:00 +0800

    add vue2-org-tree module.

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import App from './App.vue'
 import './plugins/element.js'
 import router from './router'
 import 'element-ui/lib/theme-chalk/index.css'
-import Vue20rgTree from 'vue2-org-tree'
+import Vue2OrgTree from 'vue2-org-tree'
 
-Vue.use(Vue20rgTree)
+Vue.use(Vue2OrgTree)
 
 Vue.config.productionTip = false
 
